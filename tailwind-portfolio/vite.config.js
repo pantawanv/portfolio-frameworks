2e9c885959624d198d640d70b59c5fcc6baabc33
Pantawan Vongvattanakun (Pall) <pantawan@live.dk>
2026-09-24 23:31:34 +0200

    Update Vite base path for Tailwind deployment

--- tailwind-portfolio/vite.config.js
+++ tailwind-portfolio/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-frameworks/',
+  base: '/portfolio-frameworks/tailwind/',
   plugins: [
     tailwindcss(),
     react()
