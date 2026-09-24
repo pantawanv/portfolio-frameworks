25af01e9a534b1c3c66430683ca18c7c748fd433
Pantawan Vongvattanakun (Pall) <pantawan@live.dk>
2026-09-24 22:40:13 +0200

    Add Tailwind GitHub Pages deployment

--- tailwind-portfolio/vite.config.js
+++ tailwind-portfolio/vite.config.js
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-frameworks/tailwind/',
   plugins: [
     tailwindcss(),
-    react()]
-    ,
+    react()
+  ],
 })
